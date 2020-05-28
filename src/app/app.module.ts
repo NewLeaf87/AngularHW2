d82db46b87fcc6d3f1e6d230d016bdf410bc2d17
larell mcbride <mcbridelarell@gmail.com>
2020-05-28 07:54:27 -0400

    Angular HW2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RightPageComponentComponent } from './right-page-component/right-page-component.component';
+import { LeftPageComponentComponent } from './left-page-component/left-page-component.component';
+import { AboutPageComponentComponent } from './about-page-component/about-page-component.component';
 
 @NgModule({
   declarations: [
     AppComponent
+    RightPageComponent,
+    LeftPageComponent,
+    AboutPageComponent,
+    RightPageComponentComponent,
+    LeftPageComponentComponent,
+    AboutPageComponentComponent,
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
